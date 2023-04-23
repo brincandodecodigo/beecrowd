aec30cb5180526a41b621543fbaa34655952dd0a
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-04-23 14:32:06 -0300

    Lê conteúdo

--- python/3166/3166.py
+++ python/3166/3166.py
@@ -4,5 +4,14 @@ def le_palavras(quantidade):
         palavras.append(input())
     return palavras
 
+
+def le_conteudo(linhas_matriz, colunas_matriz):
+    conteudo = []
+    for i in range(linhas_matriz):
+        linha = list(input())
+        conteudo.append(linha)
+    return conteudo
+
 quantidade_palavras, linhas_matriz, colunas_matriz = [int(x) for x in input().split()]
 palavras = le_palavras(quantidade_palavras)
+conteudo = le_conteudo(linhas_matriz, colunas_matriz)
