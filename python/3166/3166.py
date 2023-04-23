54c0a1096df4f9e11146a60cfde185dfd825cdf4
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-04-23 14:22:56 -0300

    Lê palavras

--- python/3166/3166.py
+++ python/3166/3166.py
@@ -1,1 +1,8 @@
+def le_palavras(quantidade):
+    palavras =[]
+    for i in range(quantidade):
+        palavras.append(input())
+    return palavras
+
 quantidade_palavras, linhas_matriz, colunas_matriz = [int(x) for x in input().split()]
+palavras = le_palavras(quantidade_palavras)
